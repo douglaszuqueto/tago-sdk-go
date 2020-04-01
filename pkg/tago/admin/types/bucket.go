55898b6931a44b79bc2ed272be361f0f9c58d15a
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2020-04-01 16:37:35 -0300

    [Admin] Adicionando tipos

---
+++ pkg/tago/admin/types/bucket.go
@@ -0,0 +1,37 @@
+package types
+
+// BucketListResponse BucketListResponse
+type BucketListResponse struct {
+	Result []Bucket `json:"result"`
+	Status bool     `json:"status"`
+}
+
+// BucketResponse BucketResponse
+type BucketResponse struct {
+	Result Bucket `json:"result"`
+	Status bool   `json:"status"`
+}
+
+// BucketList BucketList
+type BucketList struct {
+	ID      string        `json:"id"`
+	Name    string        `json:"name"`
+	Profile string        `json:"profile"`
+	Tags    []interface{} `json:"tags"`
+}
+
+// Bucket Bucket
+type Bucket struct {
+	ID                  string        `json:"id"`
+	Name                string        `json:"name"`
+	DataRetention       string        `json:"data_retention"`
+	Tags                []interface{} `json:"tags"`
+	DataRetentionIgnore []interface{} `json:"data_retention_ignore"`
+	Description         interface{}   `json:"description"`
+	LastBackup          interface{}   `json:"last_backup"`
+	LastRetention       interface{}   `json:"last_retention"`
+	Backup              bool          `json:"backup"`
+	Visible             bool          `json:"visible"`
+	CreatedAt           string        `json:"created_at"`
+	UpdatedAt           string        `json:"updated_at"`
+}
---
+++ pkg/tago/admin/types/device.go
@@ -0,0 +1,71 @@
+package types
+
+// DeviceListResponse DeviceListResponse
+type DeviceListResponse struct {
+	Result []Device `json:"result"`
+	Status bool     `json:"status"`
+}
+
+// DeviceGetResponse DeviceListResponse
+type DeviceGetResponse struct {
+	Result DeviceGet `json:"result"`
+	Status bool      `json:"status"`
+}
+
+// DeviceTag DeviceTag
+type DeviceTag struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// DeviceBucket DeviceBucket
+type DeviceBucket struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Device Device
+type Device struct {
+	ID   string      `json:"id"`
+	Name string      `json:"name"`
+	Tags []DeviceTag `json:"tags"`
+}
+
+// DeviceGet DeviceGet
+type DeviceGet struct {
+	ID             string       `json:"id"`
+	Name           string       `json:"name"`
+	Tags           []DeviceTag  `json:"tags"`
+	Bucket         DeviceBucket `json:"bucket"`
+	Connector      string       `json:"connector"`
+	Active         bool         `json:"active"`
+	ConnectorParse bool         `json:"connector_parse"`
+	Visible        bool         `json:"visible"`
+	Description    interface{}  `json:"description"`
+	ParseFunction  interface{}  `json:"parse_function"`
+	Profile        string       `json:"profile"`
+	CreatedAt      string       `json:"created_at"`
+	UpdatedAt      string       `json:"updated_at"`
+	InspectedAt    string       `json:"inspected_at"`
+	LastInput      string       `json:"last_input"`
+	LastOutput     string       `json:"last_output"`
+}
+
+// DeviceTokenResponse DeviceTokenResponse
+type DeviceTokenResponse struct {
+	Result []DeviceToken `json:"result"`
+	Status bool          `json:"status"`
+}
+
+// DeviceToken DeviceToken
+type DeviceToken struct {
+	Name              string      `json:"name"`
+	Token             string      `json:"token"`
+	Type              string      `json:"type"`
+	Permission        string      `json:"permission"`
+	SerieNumber       string      `json:"serie_number"`
+	VerificationCode  string      `json:"verification_code"`
+	LastAuthorization interface{} `json:"last_authorization"`
+	CreatedAt         string      `json:"created_at"`
+	ExpireTime        string      `json:"expire_time"`
+}
